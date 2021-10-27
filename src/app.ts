1f3e37569406f9ee5483ae805c806048accf3837
alex zimmerman <alex.zimmerman.info@gmail.com>
2021-10-27 22:54:57 +0300

    render template

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,34 @@
 // Code goes here!
+
+class ProjectInput {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLElement;
+  element: HTMLFormElement;
+
+  constructor() {
+    // will not be null and be of provided type
+    this.templateElement = document.getElementById(
+      "project-input"
+    )! as HTMLTemplateElement;
+    this.hostElement = document.getElementById("app")! as HTMLDivElement;
+
+    // content gives a reference to html between template tags
+    // method creates a copy of a Node or DocumentFragment from another document,
+    // to be inserted into the current document later.
+    const importedNode = document.importNode(
+      this.templateElement.content,
+      true
+    );
+
+    // Document fragment is a minimal Document object that has no parent
+    this.element = importedNode.firstElementChild! as HTMLFormElement;
+
+    this.attach();
+  }
+
+  private attach() {
+    this.hostElement.insertAdjacentElement("afterbegin", this.element);
+  }
+}
+
+const input = new ProjectInput();
